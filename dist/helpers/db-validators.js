546a4ab2ba78ce995fe6b47833662a8a09524b47
José Mendoza Vargas <ig_jose@msn.com>
2022-01-07 01:29:27 -0400

    Validando Roles e Email desde la BD

--- dist/helpers/db-validators.js
+++ dist/helpers/db-validators.js
@@ -1,2 +1,32 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.emailExiste = exports.esRolValido = void 0;
+const role_1 = __importDefault(require("../models/role"));
+const usuario_1 = __importDefault(require("../models/usuario"));
+const esRolValido = (rol) => __awaiter(void 0, void 0, void 0, function* () {
+    const existeRol = yield role_1.default.findOne({ rol });
+    if (!existeRol) {
+        throw new Error(`El rol ${rol} no existe en la BD`);
+    }
+});
+exports.esRolValido = esRolValido;
+const emailExiste = (correo) => __awaiter(void 0, void 0, void 0, function* () {
+    const existeE = yield usuario_1.default.findOne({ correo });
+    if (existeE) {
+        throw new Error(`El email ${correo} ya existe en la BD`);
+    }
+});
+exports.emailExiste = emailExiste;
 //# sourceMappingURL=db-validators.js.map
--- dist/routes/user.routes.js
+++ dist/routes/user.routes.js
@@ -1,36 +1,20 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
 const express_validator_1 = require("express-validator");
 const validar_campos_1 = require("../middlewares/validar-campos");
 const user_controller_1 = require("../controllers/user.controller");
-const role_1 = __importDefault(require("../models/role"));
+const db_validators_1 = require("../helpers/db-validators");
 exports.router = (0, express_1.Router)();
 exports.router.get('/', user_controller_1.usuariosGET);
 exports.router.post('/', [
     (0, express_validator_1.check)('nombre', 'El nombre es un campo Obligatorio').notEmpty(),
     (0, express_validator_1.check)('password', 'La contraseañ debe tener mas de 6 caracteres').isLength({ min: 6 }),
     (0, express_validator_1.check)('correo', 'El correo no es válido').isEmail(),
+    (0, express_validator_1.check)('correo').custom(db_validators_1.emailExiste),
     //check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
-    (0, express_validator_1.check)('rol').custom((rol) => __awaiter(void 0, void 0, void 0, function* () {
-        const existeRol = yield role_1.default.findOne({ rol });
-        if (!existeRol) {
-            throw new Error(`El rol ${rol} no existe en la BD`);
-        }
-    })),
+    (0, express_validator_1.check)('rol').custom(db_validators_1.esRolValido),
     validar_campos_1.validarCampos
 ], user_controller_1.usuariosPOST);
 exports.router.put('/:id', user_controller_1.usuariosPUT);
